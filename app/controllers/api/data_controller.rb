ecd5f788be56e4822250b420d3ba37927fe607fb
Jirles <emily.jirles@gmail.com>
2018-08-11 18:00:31 -0400

    Add routes and data controller

---
+++ app/controllers/api/data_controller.rb
@@ -0,0 +1,15 @@
+class Api::DataController < ApplicationController
+
+    def index 
+
+    end 
+
+    def show
+
+    end 
+
+    def create_comment
+        
+    end 
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do 
+    get '/sherries' => 'data#index'
+    get '/sherries/:id' => 'data#show'
+    post '/sherries/:id/comments' => 'data#create_comment'
+  end 
 end
