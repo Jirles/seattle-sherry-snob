63a2f9e03a6c5cf1f5d85388ecbdaa4f035464c4
Jirles <emily.jirles@gmail.com>
2018-08-11 19:01:53 -0400

    Add api key and .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace 'api' do 
+  namespace :api do 
     get '/sherries' => 'data#index'
     get '/sherries/:id' => 'data#show'
     post '/sherries' => 'data#create_sherries'
