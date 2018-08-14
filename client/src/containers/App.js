92d08f9dac95c1c5b59252c93d65d6b70bd71144
Jirles <emily.jirles@gmail.com>
2018-08-14 09:32:37 -0400

    Add react-router-dom, add About and SherryList routes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import {SherryList} from '../components/SherryList';
 import {NavBar} from '../components/NavBar';
 import {AboutPage} from '../components/AboutPage';
+import {BrowserRouter as Router,
+        Route} from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        This is the App Component
-        <SherryList />
-        <AboutPage />
-      </div>
+      <Router>
+        <div>
+            <NavBar />
+            This is the App Component
+          
+            <Route exact path='/' component={SherryList} />
+            <Route path='/about' component={AboutPage} />
+        </div>
+      </Router>
     );
   }
 }
