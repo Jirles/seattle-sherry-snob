4e0f0eacd7d0ef671ed3d5abc4b7303440d9c4d2
Jirles <emily.jirles@gmail.com>
2018-08-14 18:53:16 -0400

    Add table to SherryShow

--- client/src/containers/SherryShow.jsx
+++ client/src/containers/SherryShow.jsx
@@ -6,9 +6,47 @@ class SherryShow extends React.Component {
     render(){
         return(
             <div>
+                <h3>{this.props.sherry.name}</h3>
                 <img src={this.props.sherry.thumbnail_url} alt='A sherry bottle' width='420' height='320' />
                     
-                <p>{this.props.sherry.name}</p>
+                <h4><em>{this.props.sherry.tasting_note}</em></h4>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>
+                                Producer
+                            </th>
+                            <td>
+                                {this.props.sherry.producer}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Region of origin
+                            </th>
+                            <td>
+                                {this.props.sherry.origin}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Sugar content
+                            </th>
+                            <td>
+                                {this.props.sherry.sugar_content}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                Price USD
+                            </th>
+                            <td>
+                                {this.props.sherry.price}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr />
                 <CommentSection />
             </div>
         )
