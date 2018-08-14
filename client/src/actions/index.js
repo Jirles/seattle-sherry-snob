e0891fdc120f84e754f2ebd21230ed49c3a9293c
Jirles <emily.jirles@gmail.com>
2018-08-14 18:19:50 -0400

    Connect SherryShow to store, write fetchSherry action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,18 @@
 
-// this will not work with outside API call... 
-// sherries are listed under json.results 
-// but we may use rake to get outside API so no matter
 export const fetchSherries = () => {
     return (dispatch) => {
         dispatch({type: 'LOADING_SHERRY'});
         return fetch('/api/sherries')
                 .then(resp => resp.json())
                 .then(sherries => dispatch({ type: 'FETCH_SHERRIES', sherries }))
+    }
+}
+
+export const fetchSherry = (id) => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_SHERRY'});
+        return fetch(`/api/sherries/${id}`)
+                .then(resp => resp.json())
+                .then(sherry => dispatch({ type: 'FETCH_SHERRY', sherry }))
     }
 }
--- client/src/containers/SherryShow.jsx
+++ client/src/containers/SherryShow.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import {CommentSection} from '../components/CommentSection';
+import {connect} from 'react-redux';
+import { fetchSherry } from '../actions';
 
 class SherryShow extends React.Component {
     render(){
         return(
             <div>
                 <h3>Sherry Show Container</h3>
+                <p></p>
                 <CommentSection />
             </div>
         )
     }
 }
 
-export default SherryShow;
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        fetchSherryFromAPI: () => {dispatch({ fetchSherry() })}
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    
+    const sherry = state.sherries.find(sherry => sherry.id === parseInt(ownProps.match.params.sherryId, 10));
+    if (sherry){
+        return { sherry };
+        } else {
+        return { sherry: {} };
+        };
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SherryShow);
