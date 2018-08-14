84c56b09718bf47650ef7c7d30cb7fc12ac4d44a
Jirles <emily.jirles@gmail.com>
2018-08-14 09:14:31 -0400

    Add component, container directories and connected files

---
+++ client/src/components/SherryList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SherryList = (props) => {
+    return (
+        <div>
+            <h1>Browse some of the finest sherries</h1>
+            A list of Sherries
+        </div>
+    )
+}
---
+++ client/src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {SherryList} from '../components/SherryList';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This is the App Component
+        <SherryList />
+      </div>
+    );
+  }
+}
+
+export default App;
