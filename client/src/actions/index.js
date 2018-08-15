77973e5c37d9cdb07f0002c04f2b1d792389c0cc
Jirles <emily.jirles@gmail.com>
2018-08-15 12:17:28 -0400

    Enable CORS

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@
 export const fetchSherries = () => {
     return (dispatch) => {
         dispatch({type: 'LOADING_SHERRY'});
-        return fetch('/api/sherries')
+        return fetch('http://localhost:3001/api/sherries')
                 .then(resp => resp.json())
                 .then(sherries => dispatch({ type: 'FETCH_SHERRIES', sherries }))
     }
@@ -11,7 +11,8 @@ export const fetchSherries = () => {
 export const addComment = (comment) => {
     return (dispatch) => {
         dispatch({type: 'POSTING_COMMENT'});
-        return fetch('/api/comments', {method: 'POST', body: JSON.stringify(comment)})
+        return fetch('http://localhost:3001/api/comments', 
+                    {method: 'POST', body: JSON.stringify(comment)})
                 .then(resp => resp.json())
                 .then(comment => dispatch({ type: 'ADD_COMMENT', comment }))
     }
