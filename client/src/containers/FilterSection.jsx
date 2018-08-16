70283dc7e18301571b9d13b80a710db868d916a5
Jirles <emily.jirles@gmail.com>
2018-08-15 20:43:20 -0400

    Add state and event handlers to FilterSection

--- client/src/containers/FilterSection.jsx
+++ client/src/containers/FilterSection.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import {filterSherryResults} from '../actions';
-// need to connect filter to store so i can update state
 
-export const FilterSection = (props) => {
-    return(
-        <form onSubmit={props.handleSubmit}>
-            <fieldset>
-                <legend>Filter Options</legend>
-                <p>Filter by Price</p>
-                Min Price: <input type='number' min='0' step='0.01' name='minPrice' /><br />
-                Max Price: <input type='number' min='0' step='0.01' name='maxPrice' />
-                <p>Filter by Sweetness</p>
-                <label htmlFor='sweet'>Sweet:</label>
-                <input type='radio' name='sherryType' id='sweet' />
-                <label htmlFor='dry'> Dry:</label>
-                <input type='radio' name='sherryType' id='dry' />
-                <label htmlFor='both'> Both:</label>
-                <input type='radio' name='sherryType' id='both' defaultChecked/>
-                <br />
-                <input type='submit' value='Apply Filters' />
-            </fieldset>
-        </form>
-    )
-    
+export default class FilterSection extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            minPrice: '',
+            maxPrice: '',
+            sweetnessFilter: 'both'
+        }
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        filterSherryResults(this.state);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleOnSubmit}>
+                <fieldset>
+                    <legend>Filter Options</legend>
+                    <p>Filter by Price</p>
+                    Min Price: <input type='number' min='0' step='0.01' name='minPrice' onChange={this.handleChange} /><br />
+                    Max Price: <input type='number' min='0' step='0.01' name='maxPrice' onChange={this.handleChange} />
+                    <p>Filter by Sweetness</p>
+                    <label htmlFor='sweet'>Sweet:</label>
+                    <input type='radio' name='sweetnessFilter' id='sweet' value='sweet' onChange={this.handleChange} />
+                    <label htmlFor='dry'> Dry:</label>
+                    <input type='radio' name='sweetnessFilter' id='dry' value='dry' onChange={this.handleChange} />
+                    <label htmlFor='both'> Both:</label>
+                    <input type='radio' name='sweetnessFilter' id='both' value='both' onChange={this.handleChange} />
+                    <br />
+                    <input type='submit' value='Apply Filters' />
+                </fieldset>
+            </form>
+        )
+    }
 }
