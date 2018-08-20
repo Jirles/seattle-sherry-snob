fa3e156c7e24d1628b62ec012e5e993b6120e297
Jirles <emily.jirles@gmail.com>
2018-08-20 14:12:30 -0400

    Stub out content on Sherry101 component

--- client/src/components/Sherry101.jsx
+++ client/src/components/Sherry101.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
 export const Sherry101 = () => {
-    
+    return(
+        <div>
+            <h1>Sherry 101</h1>
+            <blockquote><em>If God had not made Sherry, how imperfect his work would have been. <small> - Benito Perez Galdos</small></em></blockquote>
+
+            <p>
+                Sherry is a fortified wine, crafted in the area around Jerez de la Frontera -- often seen shortened to Jerez -- in the southern autonomous region of Spain known as Andalusia. Contrary to popular belief, sherry is more than a sweet desert wine meant for post-dinner consumption. Much like its country of origin, sherry is multi-faceted, coming in seven varieties ranging from crisp, dry fino to dark, sweet Pedro Ximenez.
+                
+                <img alt='Photo depicting different styles of sherry' />
+                
+            </p>
+            <p>
+                <h4>Creating a bottle of sherry</h4>
+                
+                What makes sherry special? First...
+                
+                Sherries are aged in a unique system called the solera, where barrels of fortified wines sit for years at ambient temperatures in a stacked formation. The . Portions of the wine are periodically removed from the oldest barrels for bottling, with new stocks added to keep the solera going.
+            </p>
+            <p>
+                <h4>Types of Sherry</h4>
+
+                Maybe a table to look at the different types of sherry
+            </p>
+        </div>
+    )
 }
