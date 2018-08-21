75cd8fbc6e27016ff03e250e46fe6bcd751cfbf5
Jirles <emily.jirles@gmail.com>
2018-08-21 16:18:18 -0400

    Update navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 export const NavBar = () => {
     return (
         <div className='clearfix border-bottom' >
-            <div className='left p1' ><Link to='/' className='h3 caps text-decoration-none' >Seattle Sherry Snob<img alt='Pouring a glass of sherry' src='http://localhost:3001/api/banner-image' width='25%'/></Link></div>
-            <div className='right border-left border-right mt4 p1 align-bottom banner-link' ><Link className='text-decoration-none' to='/about'>About Us</Link></div>
-            <div className='right border-left mt4 p1 align-bottom banner-link' ><Link className='text-decoration-none' to='/sherry-101'>Sherry 101</Link></div>
+            <div className='left p1' ><Link to='/' className='h1 caps text-decoration-none' >Seattle Sherry Snob<img alt='Pouring a glass of sherry' src='http://localhost:3001/api/banner-image' width='25%'/></Link></div>
+            <div className='col-right border-left mt4 p1 banner-link' ><Link className='text-decoration-none' to='/about'>About Us</Link></div>
+            <div className='col-right mt4 p1 banner-link' ><Link className='text-decoration-none' to='/sherry-101'>Sherry 101</Link></div>
         </div>
     )
 }
