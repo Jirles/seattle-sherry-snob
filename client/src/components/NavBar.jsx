42c44a3a39a7ab6977156f3b8bff524d8d95c81a
Jirles <emily.jirles@gmail.com>
2018-08-21 11:41:13 -0400

    Edit navbar spacing, borders

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 
 export const NavBar = () => {
     return (
-        <div className='clearfix border' >
-            <div className='inline-block left border' ><Link to='/' className='h3 italic' >Seattle Sherry Snob</Link></div>
-            <div className='inline-block mr1 right border' ><Link to='/about'>About Us</Link></div>
-            <div className='inline-block right border' ><Link to='/sherry-101'>Sherry 101</Link></div>
+        <div className='clearfix border-bottom' >
+            <div className='left border-left border-right p2' ><Link to='/' className='h1 caps text-decoration-none' >Seattle Sherry Snob</Link></div>
+            <div className='right border-left border-right mt3 p1 align-bottom' ><Link className='text-decoration-none' to='/about'>About Us</Link></div>
+            <div className='right border-left mt3 p1 align-bottom' ><Link className='text-decoration-none' to='/sherry-101'>Sherry 101</Link></div>
         </div>
     )
 }
