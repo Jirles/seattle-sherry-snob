d566069c82d1836e036dc592748866191e5ef377
Jirles <emily.jirles@gmail.com>
2018-08-21 16:49:36 -0400

    Add fonts

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export const NavBar = () => {
     return (
         <div className='clearfix border-bottom' >
-            <div className='left p1' ><Link to='/' className='h1 caps text-decoration-none' >Seattle Sherry Snob<img alt='Pouring a glass of sherry' src='http://localhost:3001/api/banner-image' width='25%'/></Link></div>
+            <div className='left p1' ><Link to='/' id='title' className='h1 text-decoration-none' >Seattle Sherry Snob<img alt='Pouring a glass of sherry' src='http://localhost:3001/api/banner-image' width='25%'/></Link></div>
             <div className='col-right border-left mt4 p1 banner-link' ><Link className='text-decoration-none' to='/about'>About Us</Link></div>
             <div className='col-right mt4 p1 banner-link' ><Link className='text-decoration-none' to='/sherry-101'>Sherry 101</Link></div>
         </div>
