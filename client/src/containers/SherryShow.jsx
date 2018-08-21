66fc31dc98bdc8e4a33f781538ca12d22d6a1852
Jirles <emily.jirles@gmail.com>
2018-08-21 13:10:15 -0400

    Add styling to SherryShow

--- client/src/containers/SherryShow.jsx
+++ client/src/containers/SherryShow.jsx
@@ -6,49 +6,51 @@ class SherryShow extends React.Component {
     render(){
         const { sherry } = this.props;
         return(
-            <div>
-                <h3>{sherry.name}</h3>
-                <img src={sherry.thumbnail_url} alt='A sherry bottle' width='320' height='320' />
-                    
-                <h4><em>{sherry.tasting_note}</em></h4>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>
-                                Producer
-                            </th>
-                            <td>
-                                {sherry.producer}
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                Region of origin
-                            </th>
-                            <td>
-                                {sherry.origin}
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                Sugar content
-                            </th>
-                            <td>
-                                {sherry.sugar_content}
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                Price USD
-                            </th>
-                            <td>
-                                {sherry.price}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <hr />
+            <div className='flex' >
+                <section className='col-6 mx-auto my2'>
+                    <div className='h3'>{sherry.name}</div>
+                    <img src={sherry.thumbnail_url} alt='A sherry bottle' className='fit inline mb0' />
+                    <div className='col-5 h4 italic inline-block pb3'>{sherry.tasting_note}</div>
+                    <table className='table'>
+                        <tbody>
+                            <tr>
+                                <th className='border-bottom border-right pr1'>
+                                    Producer
+                                </th>
+                                <td className='border-bottom p1'>
+                                    {sherry.producer}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className='border-bottom border-right pr1'>
+                                    Region of origin
+                                </th>
+                                <td className='border-bottom p1'>
+                                    {sherry.origin}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className='border-bottom border-right pr1'>
+                                    Sugar content
+                                </th>
+                                <td className='border-bottom p1'>
+                                    {sherry.sugar_content}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className='border-bottom border-right'>
+                                    Price USD
+                                </th>
+                                <td className='border-bottom p1'>
+                                    {sherry.price}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <section className='ml2 col-4 overflow-scroll my2'>
                 <CommentSection comments={sherry.comments} sherry_id={sherry.id}/>
+                </section>
             </div>
         )
     }
